9bebe3d3f015b4288152e50c7c11fee247688660
Automation51D <51DCI@51Degrees.com>
2023-07-01 00:17:43 +0000

    REF: Updated copyright.

--- examples/cloud/web.py
+++ examples/cloud/web.py
@@ -1,6 +1,6 @@
 # *********************************************************************
 # This Original Work is copyright of 51 Degrees Mobile Experts Limited.
-# Copyright 2022 51 Degrees Mobile Experts Limited, Davidson House,
+# Copyright 2023 51 Degrees Mobile Experts Limited, Davidson House,
 # Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
 #
 # This Original Work is licensed under the European Union Public Licence
@@ -13,18 +13,12 @@
 # amended by the European Commission) shall be deemed incompatible for
 # the purposes of the Work and the provisions of the compatibility
 # clause in Article 5 of the EUPL shall not apply.
-# 
-# If using the Work as, or as part of, a network application, by 
+#
+# If using the Work as, or as part of, a network application, by
 # including the attribution notice(s) required under Article 5 of the EUPL
-# in the end user terms of the application under an appropriate heading, 
+# in the end user terms of the application under an appropriate heading,
 # such notice(s) shall fulfill the requirements of that article.
 # ********************************************************************* 
-
-## @example cloud/web.py
-# 
-# @include{doc} example-web-integration.txt
-# 
-# @include{doc} example-require-resourcekey.txt
 
 from fiftyone_location.location_pipelinebuilder import LocationPipelineBuilder
 from fiftyone_pipeline_core.web import webevidence
