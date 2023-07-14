de7eecfc1b7daea356f5dd3ee19e622e2b91a9d3
Alexander Popel <alexander.popel@postindustria.com>
2023-07-14 16:13:10 +0300

    Support CI build without a resource key

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -5,6 +5,11 @@ param (
     [Hashtable]$Keys
 )
 
+if (!$Keys.TestResourceKey) {
+    Write-Output "::warning file=$($MyInvocation.ScriptName),line=$($MyInvocation.ScriptLineNumber),title=No Resource Key::No resource key was provided, so integration tests will not run."
+    return
+}
+
 ./python/run-integration-tests.ps1 -RepoName $RepoName -Packages . -Keys $Keys
 
 exit $LASTEXITCODE
